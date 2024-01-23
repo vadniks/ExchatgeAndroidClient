32ee59208a2408b3d89f179441ca83ac7d5aef52
vadniks <whatever@fake.some>
2024-01-23 18:15:49 +0300

    UI

--- app/src/main/kotlin/org/exchatge/MainActivity.kt
+++ app/src/main/kotlin/org/exchatge/MainActivity.kt
@@ -4,31 +4,13 @@ package org.exchatge
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import org.exchatge.pages.LogInRegisterPage
 import org.exchatge.ui.theme.ExchatgeTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,64 +22,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun LogInRegisterPanel() {
-    val paddingModifier = Modifier.padding(2.5f.dp)
-
-    var username by rememberSaveable { mutableStateOf("") }
-    var password by rememberSaveable { mutableStateOf("") }
-
-    @Composable
-    fun textField(
-        value: String,
-        label: Int,
-        password: Boolean,
-        onValueChange: (String) -> Unit
-    ) {
-        return TextField(
-            value = value,
-            onValueChange = onValueChange,
-            modifier = paddingModifier,
-            label = { Text(stringResource(label)) },
-            singleLine = true,
-            visualTransformation =
-                if (!password) VisualTransformation.None
-                else PasswordVisualTransformation()
-        )
-    }
-
-    Column(
-        modifier = Modifier.fillMaxWidth(0.75f),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            stringResource(R.string.appName),
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = paddingModifier
-        )
-        textField(username, R.string.username, false) { username = it }
-        textField(password, R.string.password, true) { password = it }
-        Row(
-            Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Button(
-                onClick = {},
-                modifier = paddingModifier
-            ) {
-                Text(stringResource(R.string.logIn))
-            }
-            Button(
-                onClick = {},
-                modifier = paddingModifier
-            ) {
-                Text(stringResource(R.string.register))
-            }
-        }
-    }
-}
+private const val CURRENT_PANEL = 0 // TODO: debug only
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
@@ -107,12 +32,8 @@ fun Preview() {
             modifier = Modifier.fillMaxSize(), //.border(1.0f.dp, color = Color.Black, RoundedCornerShape(1.0f.dp)),
             color = MaterialTheme.colorScheme.background
         ) {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                LogInRegisterPanel()
+            when (CURRENT_PANEL) {
+                0 -> LogInRegisterPage()
             }
         }
     }
---
+++ app/src/main/kotlin/org/exchatge/pages/LogInRegisterPage.kt
@@ -0,0 +1,96 @@
+
+package org.exchatge.pages
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.exchatge.R
+
+@Composable
+fun LogInRegisterPage() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        LogInRegisterPanel()
+    }
+}
+
+@Composable
+private fun LogInRegisterPanel() {
+    val paddingModifier = Modifier.padding(2.5f.dp)
+
+    var username by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+
+    @Composable
+    fun textField(
+        value: String,
+        label: Int,
+        password: Boolean,
+        onValueChange: (String) -> Unit
+    ) {
+        return TextField(
+            value = value,
+            onValueChange = onValueChange,
+            modifier = paddingModifier,
+            label = { Text(stringResource(label)) },
+            singleLine = true,
+            visualTransformation =
+            if (!password) VisualTransformation.None
+            else PasswordVisualTransformation()
+        )
+    }
+
+    Column(
+        modifier = Modifier.fillMaxWidth(0.75f),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            stringResource(R.string.appName),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = paddingModifier
+        )
+        textField(username, R.string.username, false) { username = it }
+        textField(password, R.string.password, true) { password = it }
+        Row(
+            Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Button(
+                onClick = {},
+                modifier = paddingModifier
+            ) {
+                Text(stringResource(R.string.logIn))
+            }
+            Button(
+                onClick = {},
+                modifier = paddingModifier
+            ) {
+                Text(stringResource(R.string.register))
+            }
+        }
+    }
+}
