875947fd078b70e661f03e511a79f708cefdd02b
vadniks <whatever@fake.some>
2024-01-26 20:29:47 +0300

    UI

--- app/src/main/kotlin/org/exchatge/pages/ConversationPage.kt
+++ app/src/main/kotlin/org/exchatge/pages/ConversationPage.kt
@@ -2,7 +2,9 @@
 package org.exchatge.pages
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -12,22 +14,23 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.exchatge.R
 import java.text.SimpleDateFormat
 
-private val opponentUsername = "User"
+private val opponentUsername = "User" // TODO: debug only
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -65,27 +68,31 @@ fun ConversationPage() = Scaffold(
 
 @SuppressLint("SimpleDateFormat")
 @Composable
-private fun Message(timestamp: Long, from: String?, text: String) = ListItem(
-    leadingContent = {
-        Text(
-            text = from ?: stringResource(R.string.you),
-            fontSize = 16.sp,
-            color = MaterialTheme.colorScheme.secondary
-        )
-    },
-    headlineContent = {
+private fun Message(timestamp: Long, from: String?, text: String) = Box(
+    contentAlignment = if (from != null) Alignment.CenterStart else Alignment.CenterEnd,
+    modifier = Modifier.fillMaxWidth().padding(5.dp)
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth(.5f)
+    ) {
         Text(
             text = text,
             textAlign = TextAlign.Justify
         )
-    },
-    supportingContent = {
-        Text(
-            text = SimpleDateFormat("hh:mm:ss MMM-DD-yyyy").format(timestamp),
-            fontSize = 14.sp,
-            fontStyle = FontStyle.Italic,
-            color = MaterialTheme.colorScheme.secondary
-        )
-    },
-    modifier = Modifier.fillMaxWidth(.5f)
-)
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = SimpleDateFormat("hh:mm:ss MMM-DD-yyyy").format(timestamp),
+                fontSize = 12.sp,
+                fontStyle = FontStyle.Italic,
+                color = MaterialTheme.colorScheme.secondary,
+                modifier = Modifier.padding(end = 5.dp)
+            )
+            Text(
+                text = from ?: stringResource(R.string.you),
+                fontSize = 14.sp,
+                color = MaterialTheme.colorScheme.secondary,
+                modifier = Modifier.padding(start = 5.dp)
+            )
+        }
+    }
+}
