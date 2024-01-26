5b0bae70f1ea4664a1573ed13805a03285639de6
vadniks <whatever@fake.some>
2024-01-26 14:37:15 +0300

    UI

--- app/src/main/kotlin/org/exchatge/pages/UsersListPage.kt
+++ app/src/main/kotlin/org/exchatge/pages/UsersListPage.kt
@@ -2,26 +2,36 @@
 package org.exchatge.pages
 
 import android.annotation.SuppressLint
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -78,7 +88,76 @@ fun UsersListPage() = Scaffold(
             modifier = Modifier.fillMaxSize(),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-
+            LazyColumn {
+                items(10) { // TODO: debug only
+                    UserInfo(
+                        id = it,
+                        name = "User$it",
+                        online = it % 2 == 0,
+                        conversationExists = it % 3 == 0
+                    )
+                }
+            }
         }
     }
+}
+
+@Composable
+private fun UserInfo(
+    id: Int,
+    name: String,
+    online: Boolean,
+    conversationExists: Boolean
+) {
+    val fontSize = 16.sp
+
+    ListItem(
+        leadingContent = {
+            Text(
+                text = id.toString(),
+                fontSize = fontSize,
+                fontStyle = FontStyle.Italic,
+                color = MaterialTheme.colorScheme.secondary,
+                modifier = Modifier.fillMaxWidth(.1f)
+            )
+        },
+        headlineContent = {
+            Text(
+                text = name,
+                fontSize = fontSize,
+                modifier = Modifier.fillMaxWidth(.5f)
+            )
+        },
+        supportingContent = {
+            Text(
+                text = stringResource(if (online) R.string.online else R.string.offline),
+                fontSize = 14.sp,
+                color = MaterialTheme.colorScheme.secondary,
+                modifier = Modifier.fillMaxWidth(.5f)
+            )
+        },
+        trailingContent = {
+            @Composable
+            fun Content() {
+                @Composable
+                fun Button(text: Int) = TextButton(onClick = {}) {
+                    Text(
+                        text = stringResource(text),
+                        fontSize = 12.sp,
+                        modifier = Modifier.fillMaxWidth(.5f)
+                    )
+                }
+
+                if (!conversationExists)
+                    Button(R.string.startConversation)
+                else {
+                    Button(R.string.continueConversation)
+                    Button(R.string.deleteConversation)
+                }
+            }
+
+            (LocalConfiguration.current.orientation == Configuration.ORIENTATION_PORTRAIT)
+                .also { if (it) Column { Content() } else Row { Content() } }
+        }
+    )
 }
