be3d2f4a3db135e96a3ed03b19cba182260694de
vadniks <whatever@fake.some>
2024-01-26 20:49:48 +0300

    UI

--- app/src/main/kotlin/org/exchatge/pages/ConversationPage.kt
+++ app/src/main/kotlin/org/exchatge/pages/ConversationPage.kt
@@ -2,10 +2,10 @@
 package org.exchatge.pages
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -18,9 +18,13 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -32,6 +36,7 @@ import org.exchatge.R
 import java.text.SimpleDateFormat
 
 private val opponentUsername = "User" // TODO: debug only
+private var enteredText by mutableStateOf("")
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -60,10 +65,24 @@ fun ConversationPage() = Scaffold(
         )
     }
 ) { paddingValues ->
-    LazyColumn(modifier = Modifier.fillMaxSize().padding(top = paddingValues.calculateTopPadding())) {
-        items(10) { // TODO: debug only
-            Message(System.currentTimeMillis(), if (it % 2 == 0) "User$it" else null, "Text$it")
+    Column(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxHeight(.9f)
+                .fillMaxWidth()
+                .padding(top = paddingValues.calculateTopPadding())
+        ) {
+            items(10) { // TODO: debug only
+                Message(System.currentTimeMillis(), if (it % 2 == 0) "User$it" else null, "Text$it")
+            }
         }
+        TextField(
+            value = enteredText,
+            onValueChange = { enteredText = it },
+            modifier = Modifier.fillMaxSize().padding(5.dp),
+            label = { Text(stringResource(R.string.message)) },
+            singleLine = false
+        )
     }
 }
 
